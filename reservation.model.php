0b521130956bb752225888964fde6ada672e99d1
Yohann Dubuis <yohann.dubuis@lapiscine.pro>
2025-04-14 15:12:19 +0200

    creation function construc

--- reservation.model.php
+++ reservation.model.php
@@ -20,25 +20,37 @@ class Reservation { // creation d'une class reservation
 
 	public $nightPrice; // creation de la variable prix de la nuit  dans la classe reservation
 
+    public $bookedAt; // creationd de la variable determinant la date de commande de la reservation.
 
 	public $status; // creation de la variable status dans la classe reservation
 
 
 	public $cleaningOption; // creation de la variable prix du ménage dans la classe reservation
 
-}
-$reservation->firstName = "Emmanuel"; //ici la variable firstname deviens un prenom 
-$reservation->name = "Macron"; // ici la variable name devient un nom de famille.
-$reservation->place = "2017 Autoroute de la ruine"; //ici nous avons le lieu du séjour 
-$reservation->startDate = new DateTime("25-04-15"); // ici on donne une valeur au début du sejour via startDate
-$reservation->endDate = new DateTime("25-05-17"); // ici on y donne la fin du séjour 
-$reservation->cleaningOption = true; //on demande de prendre en compte l'option de menage
+    public function __construct() {
 
-$reservation->nightPrice = 1000; //on determine le prix de la nuit 
+
+$this->firstName = "Emmanuel"; //ici la variable firstname deviens un prenom 
+$this->name = "Macron"; // ici la variable name devient un nom de famille.
+$this->place = "2017 Autoroute de la ruine"; //ici nous avons le lieu du séjour 
+$this->startDate = new DateTime("25-04-15"); // ici on donne une valeur au début du sejour via startDate
+$this->endDate = new DateTime("25-05-17"); // ici on y donne la fin du séjour 
+$this->cleaningOption = true; //on demande de prendre en compte l'option de menage
+
+$this->nightPrice = 1000; //on determine le prix de la nuit 
 
 // on donne une une valeur via une variable et un calcul automtique du prix total du séjour.
-$totalPrice = (($reservation->endDate->getTimestamp() - $reservation->startDate->getTimestamp()) / (3600 * 24) * $reservation->nightPrice) + 5000;
+$totalPrice = (($this->endDate->getTimestamp() - $this->startDate->getTimestamp()) / (3600 * 24) * $this->nightPrice) + 5000;
 
-$reservation->totalPrice = $totalPrice; // ici dans la class reservation on vient chercher le total price a qui on attribut la valeur du calcul juste au dessus
-$reservation->bookedAt = new DateTime(); // la date de reservation 
-$reservation->status = "CART"; // status de la commande.
+$this->totalPrice = $totalPrice; // ici dans la class reservation on vient chercher le total price a qui on attribut la valeur du calcul juste au dessus
+$this->bookedAt = new DateTime(); // la date de reservation 
+$this->status = "CART"; // status de la commande.
+//$this peut etre utilisé a l'interieur d'une class. ce qui permet de ne pas appeler la class sans arret et juste mettre $this
+    }
+
+}
+
+$reservation = new Reservation();
+var_dump($reservation);
+?>
+
